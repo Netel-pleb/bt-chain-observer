ad8c3bfc81da77489dbb53cebbc6046e0e1cdf0a
nesta-onur111 <cs.eros111@gmail.com>
2024-10-09 11:36:12 -0300

    fix : change db management

--- run.py
+++ run.py
@@ -1,62 +1,74 @@
-# This script orchestrates the process of monitoring a GitHub repository.
-# It fetches repository data, compares current and previous states, generates reports on pull requests and branches,
-# and posts these reports to Discord.
-
 import os
 import time
-import threading
-import subprocess
 from datetime import datetime
-from observing.bot.bot import post_to_discord
-from observing.observer.observer import observer_block
-from observing.utils.get_coldkeys import find_owner_coldkey
-from main import check_thread_status
-import os
+import threading
+import logging
 from dotenv import load_dotenv
-import sentry_sdk
+from chain_observer.bot.discord_report import post_to_discord
+from chain_observer.bot.bt_chain_observer import BtChainObserver
+from db_manage.db_manager import db_manager
+from chain_observer.utils.check_thread_status import check_update_thread_status
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+COLDKEY_SWAP_DISCORD_WEBHOOK_URL = os.getenv('COLDKEY_SWAP_DISCORD_WEBHOOK_URL')
+DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL = os.getenv('DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL')
+chain_observer = BtChainObserver()
 
 def run_update_owner_coldkey_function():
     """Runs the find_owner_coldkey function in a new thread."""
+    try:
+        with open('config/thread_status.status', 'w') as f:
+            f.write('running')
+        db_manager.update_whole_owner_coldkeys()
+    except Exception as e:
+        logging.error(f"Error in updating owner coldkey: {e}")
+    finally:
+        with open('config/thread_status.status', 'w') as f:
+            f.write('not running')
 
-    with open('thread_status.status', 'w') as f:
-        f.write('running')
-    find_owner_coldkey()
-    with open('thread_status.status', 'w') as f:
-        f.write('not running')
+def run_bot():
+    """Process and send reports to Discord."""
+    try:
+        (report_swap_coldkey, report_dissolve_network, report_vote, 
+         dissolved_subnet_report, swapped_coldkey_report, 
+         should_update_owner_table) = chain_observer.bt_block_observer()
         
+        if should_update_owner_table:
+            thread_status = check_update_thread_status()
+            if thread_status == 'not running':
+                update_owner_thread = threading.Thread(target=run_update_owner_coldkey_function)
+                update_owner_thread.start()
+            else:
+                logging.info("Update owner coldkey function is already running.")
+
+        reports = [
+            (report_swap_coldkey, COLDKEY_SWAP_DISCORD_WEBHOOK_URL),
+            (report_dissolve_network, DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL),
+            (dissolved_subnet_report, DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL),
+            (report_vote, COLDKEY_SWAP_DISCORD_WEBHOOK_URL),
+            (swapped_coldkey_report, COLDKEY_SWAP_DISCORD_WEBHOOK_URL),
+        ]
+        
+        # Post reports to Discord only if they have values
+        for report, webhook_url in reports:
+            if report:
+                post_to_discord(report, webhook_url)
+    except Exception as e:
+        logging.error(f"Error during running bot: {e}")
+
 def run():
-
+    """Main function to run the bot process."""
+    time_now = datetime.now()
     start_time = time.time()
-    print(f"Start time: {datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}")
-
-    load_dotenv()
+    logging.info(f"Bot process started at {time_now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}.")
     
-    COLDKEY_SWAP_DISCORD_WEBHOOK_URL = os.getenv('COLDKEY_SWAP_DISCORD_WEBHOOK_URL')
-    DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL = os.getenv('DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL')
-
-    report_swap_coldkey, report_dissolve_network, report_vote, dissloved_subnet_resport, swapped_coldkey_report, should_update_owner_table = observer_block()
-
-    # Run the get_coldkey.py script in a new thread if the owner database should be updated.
-    if should_update_owner_table:
-        thread_status = check_thread_status()
-        if thread_status == 'not running':
-            update_owner_thread = threading.Thread(target=run_update_owner_coldkey_function)
-            update_owner_thread.start()
-        else:
-            print("Update owner coldkey function is already running.")
-    
-    post_to_discord(report_swap_coldkey, COLDKEY_SWAP_DISCORD_WEBHOOK_URL)
-    post_to_discord(report_dissolve_network, DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL)
-    post_to_discord(dissloved_subnet_resport, DISSOLVE_NETWORK_DISCORD_WEBHOOK_URL)
-    post_to_discord(report_vote, COLDKEY_SWAP_DISCORD_WEBHOOK_URL)
-    post_to_discord(swapped_coldkey_report, COLDKEY_SWAP_DISCORD_WEBHOOK_URL)
+    run_bot()
     
     end_time = time.time()
-    print(f"End time: {datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}")
-    print(f"Time consumed: {end_time - start_time:.3f} seconds")
-
+    logging.info(f"Process completed in {end_time - start_time:.3f} seconds.")
 
 if __name__ == "__main__":
-
     run()
-    
